cc3838e8086c4a7c6062470c0eca921cddffee1c
ParkYoungJun123 <120649627+ParkYoungJun123@users.noreply.github.com>
2023-02-07 11:17:02 +0900

    2번째 커밋

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,5 +6,5 @@ import styles from "@/styles/Home.module.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <>hello world</>;
+  return <>hello world 2</>;
 }
